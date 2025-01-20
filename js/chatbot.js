7c70f3d5ca3bdc6ebcb7f5d33573e9d0ee4d7702
Nandini1009 <nandinigautam120897@gmail.com>
2025-01-21 00:35:26 +0530

    Add files via upload

---
+++ js/chatbot.js
@@ -0,0 +1,80 @@
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 1000 + (Math.random() * 20) * 100);
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+var Fake = [
+  'Welcome to Kidzera Chat! How can I help you? ',
+  'Please Enter Your Order Id. ',
+  'Your Product has been shipped and will be delivered on time.',
+  'Should you need any further assistance, feel free to connect with us again!',
+  'Your feedback is really important to us. Please share your experience based on our conversation by completing the survey that you will get after this chat session.<div class="loading-img"> <img src="imgs/rating.png" /></div>',
+  'We appreciate your time and patience. Thank you for choosing Kidzera. <i>Play, Learn and Grow Together</i>Bye and Take care.',
+  'How did your chat go ?<input type="text" class="form-control oracle-search" name="query"  placeholder="Please rate us: 0/5">',
+  'Tell us what you think?',
+  'Thanks for your feedback. If you need more help, you can ask another question at any time.',
+  ':)'
+]
+
+function fakeMessage() {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="imgs/chatbot.webp" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="imgs/chatbot.webp" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
+}
